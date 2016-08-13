53239e7e1f6cba0c0136f8efda2d68b5a885a108
engrnazmulbari <engr.nazmulbari@gmail.com>
2016-08-12 20:12:21 -0500

    indeex.jsp added

--- ServletJSPExample/src/com/example/tutorial/ServletExample.java
+++ ServletJSPExample/src/com/example/tutorial/ServletExample.java
@@ -13,22 +13,20 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class ServletExample extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public ServletExample() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		PrintWriter out = response.getWriter();
-		out.println("Hello Servlet");
+		
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		
+		out.println("Hello " + firstName+" "+lastName);
 	}
 
 }
